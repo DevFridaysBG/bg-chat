03c1632a7fe871113619a547999e7b7989135242
stoychev <stoychev@lexoro.ai>
2020-04-03 17:12:34 +0300

    initial commit

---
+++ src/main/java/bg/chat/app/Greeting.java
@@ -0,0 +1,12 @@
+package bg.chat.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+    private String content;
+}
